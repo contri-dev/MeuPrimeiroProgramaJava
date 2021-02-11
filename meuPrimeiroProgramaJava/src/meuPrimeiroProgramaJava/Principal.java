b63cd1b07f420394ce7e6aa9453e43473eb610f4
Luiz Henrique Contri <luiz.contri123@gmail.com>
2021-02-10 21:00:31 -0300

    Add files via upload

---
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -0,0 +1,32 @@
+/**
+ * meu primeiro programa Java
+ * 
+ * Este programa mostra ao aluno:
+ * 1) Que um programa java é construido dentro de um projeto Java
+ * 2) Todo programa java deve ter uma classe com um método main.
+ * 3) Como exibir informações no terminal (console) do Java.
+ * 4) Como ler uma string do terminal (console) do Java.
+ * 
+ */
+package meuPrimeiroProgramaJava;
+
+import java.util.Scanner;
+
+/**
+ * @author 55119
+ * data: 03/02/2021
+ */
+public class Principal { // Classe que contem o metodo principal.
+	public static void main(String[] args) { // Este é o metodo principal
+		System.out.println("Olá amigo!\nQual é o seu nome?");
+		
+		// Ler uma string do console usando a biblioteca Scanner
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+		
+		// Exibir um string com printf
+		System.out.printf("Ola %s!", nome);
+		in.close();
+	}
+
+}
