ef8abaa4cbc1c6c4c40ffdb3321a3b5a8bb6da24
Luiz Henrique Contri <luiz.contri123@gmail.com>
2021-02-10 21:08:06 -0300

    Alteração no ultimo comentário

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -2,9 +2,9 @@
  * meu primeiro programa Java
  * 
  * Este programa mostra ao aluno:
- * 1) Que um programa java é construido dentro de um projeto Java
- * 2) Todo programa java deve ter uma classe com um método main.
- * 3) Como exibir informações no terminal (console) do Java.
+ * 1) Que um programa java Ã© construido dentro de um projeto Java
+ * 2) Todo programa java deve ter uma classe com um mÃ©todo main.
+ * 3) Como exibir informaÃ§Ãµes no terminal (console) do Java.
  * 4) Como ler uma string do terminal (console) do Java.
  * 
  */
@@ -20,14 +20,14 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		
-		// Saldação e pergunta o nome
-		System.out.println("Olá amigo!\nQual é o seu nome?");
+		// SaldaÃ§Ã£o e pergunta o nome
+		System.out.println("OlÃ¡ amigo!\nQual Ã© o seu nome?");
 		
 		// Ler o nome
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Saldação específica
+		// SaldaÃ§Ã£o especÃ­fica para o nome lido
 		System.out.printf("Ola %s!", nome);
 		in.close();
 	}
